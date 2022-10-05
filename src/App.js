11db4884fe56594cb4466618f2ae779f190f3e93
hoon2 <sanghoon.engineer@gmail.com>
2022-10-06 08:00:43 +0900

    what-is-jsx 과제 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  return (
+    <h1>jsx규칙을 이용해서 다음과 같은 버그를 해결해주세요</h1>
+    <h2>오늘 배운 3가지 규칙 모두 활용하면 됩니다.<h2>
+    <h3 class="homework">
+      jsx의 3가지 규칙을 잘 기억해서 위 문제를 해결해보세요 해결이 완료가 되면
+      npm run start 실행을했을때 잘 나옵니다.
+    </h3>
+  );
 }
 
 export default App;
